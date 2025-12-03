d8f63a24baefe1117d47274b0bdee3e1cfcbcb43
emilyfranceskolar@gmail.com <emilyfranceskolar@gmail.com>
2025-12-03 14:45:19 +0100

    changed structure, trying to get my code to be more reusable

--- script.js
+++ script.js
@@ -1,58 +1,87 @@
-//anropar en callback till main
+//objekt för att skapa och lagra spelets innehåll
+let content = {
+  // currentScene: "scene1",
+  scene1: {
+    title: "The Wolf",
+    text: `You step into the forrest, immediately meet a giant wolf. You don't see <br /> 
+   any weapons nearby, and you need to make decision quickly. What do you do?`,
+    image: "./images/wolf.jpg",
+    options: [
+      {
+        text: "Scream loudly and run",
+      },
+      {
+        text: "Wait for the wolf to make a move",
+      },
+      {
+        text: "Ask the wolf for help",
+      },
+      {
+        text: "Ignore the wolf, keep moving",
+      },
+    ],
+  },
+};
+
+//anropar en callback till main när sidan har laddats
 window.addEventListener("DOMContentLoaded", main);
 
-let rooms = ["", "The Mountain", "The Creek", "Hillside", ""];
-const backpack = [];
-
-const ruleBtnHtml = document.getElementById("rulesBtn");
-const startBtnHtml = document.getElementById("startBtn");
+// spelaren ska kunna plocka upp (lagre) och använda (ta bort) objekt under spelet
+let backpack = {};
 
 function main() {
-  rulesBtn.addEventListener("click", showRules); //call function to see instructions how to play
-  startBtn.addEventListener("click", startGame); //call function to startGame
+  //variabler för de 2 knapparna på sidan från allra första början
+  const btnHtml = document.querySelector(".btn");
+  const startBtnHtml = document.getElementById("startBtn");
+  //anropar em callback till funktionenn showInstructions när man klickar på rulesBtnHtml
+  btnHtml.addEventListener("click", showInstructions);
+  //anropar en callback till funktionen startGame när man klickar på startBtnHtml
+  startBtnHtml.addEventListener("click", startGame);
 }
 
-function showRules() {}
+function showInstructions() {}
+
 function startGame() {
   clearContent();
+  renderNewContent();
 }
 
 function clearContent() {
-  const titleHtml = document.querySelector("h2");
-  titleHtml.remove();
-  changeImage();
+  const pageTitleHtml = document.querySelector(".page-title");
+  const textHtml = document.querySelector(".page-text");
+  const btnsHtml = document.querySelector(".btn-grid");
+
+  pageTitleHtml.remove();
+  textHtml.remove();
+  btnsHtml.remove();
 }
 
-function changeImage() {
-  const containerHtml = document.querySelector(".container");
+function renderNewContent() {
+  const contentHtml = document.querySelector(".content");
+  // const imageHtml = document.createElement("img");
+  // imageHtml.id = "#content-image";
+  // imageHtml.src = content.scene1.image;
+  // contentHtml.append(imageHtml);
 
-  let imageCount = 0;
-  const images = ["./images/wolf.jpg", "./images/svamp2.jpg"];
-  const imageHtml = document.querySelector("img");
+  const titleHtml = document.createElement("h2");
+  titleHtml.className = "page-title";
+  titleHtml.innerHTML = content.scene1.title;
+  contentHtml.append(titleHtml);
 
-  imageHtml.src = images[imageCount];
-  containerHtml.innerHTML = " ";
-  containerHtml.append(imageHtml);
+  const paragraphHtml = document.createElement("p");
+  paragraphHtml.className = "page-text";
+  paragraphHtml.innerHTML = content.scene1.text;
+  contentHtml.append(paragraphHtml);
 
-  imageCount++;
-  if (imageCount >= images.length) {
-    imageCount = 0;
+  const btnGridHtml = document.createElement("div");
+  btnGridHtml.className = "btn-grid";
+
+  let btnHtml;
+  for (let i = 0; i < content.scene1.options.length; i++) {
+    btnHtml = document.createElement("button");
+    btnHtml.className = "btn";
+    btnHtml.innerHTML = content.scene1.options[i].text;
+    btnGridHtml.append(btnHtml);
   }
-  updatePage();
-}
-
-function updatePage() {
-  const containerHtml = document.querySelector(".container");
-  const headerHtml = document.querySelector("header");
-  const newTitleHtml = document.createElement("h2");
-  const newParagraphHtml = document.createElement("p");
-  newParagraphHtml.className = "page-text";
-
-  newTitleHtml.innerHTML = "The Wolf mother";
-  headerHtml.append(newTitleHtml);
-
-  newParagraphHtml.innerHTML = `You've stepped into the forrest and just like that you meet a giant wolf. <br /> 
-      For some reason you get the feeling the wolf wants to tell you something.`;
-
-  containerHtml.append(newParagraphHtml);
+  contentHtml.append(btnGridHtml);
 }
