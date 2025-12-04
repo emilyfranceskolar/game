042e3640cfbcc51df39e3f7d4fd2b1cba3a17e9c
emilyfranceskolar@gmail.com <emilyfranceskolar@gmail.com>
2025-12-04 16:28:08 +0100

    updated code structure

--- script.js
+++ script.js
@@ -1,23 +1,73 @@
 //objekt för att skapa och lagra spelets innehåll
 let content = {
-  currentScene: "scene1",
-  scene1: {
+  help: {
+    title: "how to play",
+    text: `To play this game it's simple, click on 1 of the buttons to make a decision based on the text. <br /> `,
+    image: "./images/help.jpg",
+    options: [
+      {
+        text: "Eh, I'm good",
+      },
+      {
+        text: "Ok, let's play",
+      },
+    ],
+  },
+  wolf: {
     title: "The Wolf",
     text: `You step into the forrest, immediately meet a giant wolf.<br /> 
       You don't see any weapons nearby. What do you do?`,
     image: "./images/wolf.jpg",
     options: [
       {
-        text: "Run for your life",
+        text: "ask the wolf for help",
       },
       {
-        text: "Wait for the wolf to make a move",
+        text: "run for your life",
       },
       {
-        text: "Ask the wolf for help",
+        text: "look for a weapon",
       },
       {
-        text: "Ignore the wolf, keep moving",
+        text: "quit game",
+      },
+    ],
+  },
+  cabin: {
+    title: "The Cabin",
+    text: `You see a cabin in the distance, you're not sure if the wolf is behind you or not. What do you do?`,
+    image: "./images/cabin.jpg",
+    options: [
+      {
+        text: "run to the cabin",
+      },
+      {
+        text: "eh I'm good, quit game",
+      },
+      {
+        text: "ignore the cabin",
+      },
+      {
+        text: "scream for help",
+      },
+    ],
+  },
+  tent: {
+    title: "The Campsite",
+    text: `You've come to what looks like a tent in the distance. You're not sure if the wolf is behind you or not. What do you do?`,
+    image: "./images/camper.jpg",
+    options: [
+      {
+        text: "stop and listen for sounds",
+      },
+      {
+        text: "call for help",
+      },
+      {
+        text: "look for a weapon",
+      },
+      {
+        text: "quit game",
       },
     ],
   },
@@ -27,7 +77,10 @@ let content = {
 window.addEventListener("DOMContentLoaded", main);
 
 // spelaren ska kunna plocka upp (lagre) och använda (ta bort) objekt under spelet
-let backpack = {};
+let backpack = [];
+
+// variabel för att lagra var spelare befinner sig i spelet
+let currentScene = null;
 
 function main() {
   //variabler för de 2 knapparna på sidan från allra första början
@@ -40,12 +93,15 @@ function main() {
 }
 
 function showInstructions() {
+  currentScene = content.help;
   clearContent();
+  renderNewContent(currentScene);
 }
 
 function startGame() {
+  currentScene = content.wolf;
   clearContent();
-  renderNewContent();
+  renderNewContent(currentScene);
 }
 
 function clearContent() {
@@ -54,7 +110,7 @@ function clearContent() {
   containertHtml.remove();
 }
 
-function renderNewContent() {
+function renderNewContent(currentScene) {
   //skapa en ny div (class = "container")
   const newContainerHtml = document.createElement("div");
   newContainerHtml.className = "container";
@@ -67,7 +123,8 @@ function renderNewContent() {
   newImageHtml.id = "bg-image";
 
   //anger platsen för src, lägg till i container
-  newImageHtml.src = content[content.currentScene].image;
+  newImageHtml.src = currentScene.image;
+
   newContainerHtml.append(newImageHtml);
 
   const contentHtml = document.createElement("div");
@@ -77,23 +134,39 @@ function renderNewContent() {
   const titleHtml = document.createElement("h2");
   titleHtml.className = "page-title";
 
-  titleHtml.innerHTML = content[content.currentScene].title;
+  titleHtml.innerHTML = currentScene.title;
   contentHtml.append(titleHtml);
 
   const paragraphHtml = document.createElement("p");
   paragraphHtml.className = "page-text";
-  paragraphHtml.innerHTML = content[content.currentScene].text;
+  paragraphHtml.innerHTML = currentScene.text;
   contentHtml.append(paragraphHtml);
 
   const btnGridHtml = document.createElement("div");
   btnGridHtml.className = "btn-grid";
 
   let btnHtml;
-  for (let i = 0; i < content[content.currentScene].options.length; i++) {
+  for (let i = 0; i < currentScene.options.length; i++) {
     btnHtml = document.createElement("button");
     btnHtml.className = "btn" + i;
-    btnHtml.innerHTML = content[content.currentScene].options[i].text;
+    btnHtml.innerHTML = currentScene.options[i].text;
     btnGridHtml.append(btnHtml);
   }
   contentHtml.append(btnGridHtml);
+  console.log(Object.keys(content));
+
+  getSelection();
 }
+
+function getSelection() {
+  //en nodelist skapas när alla knappar hämtas
+  const listOfButtons = document.querySelectorAll("button");
+  // console.log(listOfButtons);
+
+  //for varje knapp vill vi se vad den innehåller
+  for (const button of listOfButtons) {
+    console.log(button);
+  }
+}
+
+function gameOver() {}
